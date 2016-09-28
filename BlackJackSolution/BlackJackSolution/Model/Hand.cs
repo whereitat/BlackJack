141b717b7202fa0874bafbf5925a5da195a76129
Hyde9 <g.hedin@hotmail.com>
2016-09-28 15:26:52 +0200

    CheckHand

--- BlackJackSolution/BlackJackSolution/Model/Hand.cs
+++ BlackJackSolution/BlackJackSolution/Model/Hand.cs
@@ -9,12 +9,39 @@ namespace BlackJackSolution.Model
     public class Hand
     {
         public List<Card> handCards { set; get; }
-        public int score { set; get; }
-        public int numberOfCards { set; get; }
+        public int total { set; get; }
 
         public Hand()
         {
 
+        }
+        public int CheckHand()
+        {
+            int handTotal = 0;
+                for (int i = 0; i < handCards.Count; i++)
+                {
+
+                    handTotal += handCards[i].value;
+
+                    if (handTotal > 21)
+                    {
+                        for (int j = 0; j < handCards.Count; j++)
+                        {
+                            if(handCards[j].value == 11)
+                            {
+                                handCards[j].value = 1;
+                                handTotal += handCards[j].value - 11;
+                                break;
+                            }
+                        }
+                    }
+                }
+            this.total = handTotal;
+            return total;
+        }
+        public int getNumberOfCards()
+        {
+            return handCards.Count;
         }
     }
 }
