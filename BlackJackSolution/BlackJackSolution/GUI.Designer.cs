697557cbc4c86da8635c2a625a6fec08e325a24e
Hyde9 <g.hedin@hotmail.com>
2016-09-28 13:58:44 +0200

    Setup

--- BlackJackSolution/BlackJackSolution/GUI.Designer.cs
+++ BlackJackSolution/BlackJackSolution/GUI.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BlackJackSolution
 {
-    partial class Form1
+    partial class GUI
     {
         /// <summary>
         /// Required designer variable.
---
+++ BlackJackSolution/BlackJackSolution/Model/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackSolution.Model
+{
+    public class Card
+    {
+        public int value { set; get; }
+    }
+}
