97e73b9d8c34488c6d1126866b2fa7a815cf9a7f
Hyde9 <g.hedin@hotmail.com>
2016-09-29 15:19:24 +0200

    DBConnection Test

--- BlackJackSolution/BlackJackSolution/DAL/DBAccess.cs
+++ BlackJackSolution/BlackJackSolution/DAL/DBAccess.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlackJackSolution.Model;
 
 namespace BlackJackSolution.DAL
 {
     public class DBAccess
     {
+        public static SqlConnection Connect()
+        {
+
+            try
+            {
+                SqlConnection con = new SqlConnection("user id=test;" +
+                "password=test; server=localhost;" +
+                "Trusted_Connection=yes;" +
+                "database=BlackJackProject;" +
+                "connection timeout=5");
+
+                con.Open();
+                Console.WriteLine("Connection established");
+                return con;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString() + e.Message);
+                return null;
+            }
+        }
+        public static Deck CreateDeck()
+        {
+            try {
+                Deck newDeck = new Deck();
+                SqlConnection connection = Connect();
+                SqlCommand command = new SqlCommand("EXEC dbo.ShuffleCards", connection);
+                command.ExecuteNonQuery();
+                SqlDataReader read = command.ExecuteReader();
+
+                if (read.HasRows)
+                {
+                        while (read.Read())
+                        {
+                            Card card = new Card();
+                            card.value = read.GetInt32(read.GetOrdinal("value"));
+                            newDeck.cards.Add(card);
+                        }
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong, no objects found.");
+                }
+                return newDeck;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            
+        }
     }
 }
--- BlackJackSolution/BlackJackSolution/Program.cs
+++ BlackJackSolution/BlackJackSolution/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BlackJackSolution.Model;
 
 namespace BlackJackSolution
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -14,9 +15,19 @@ namespace BlackJackSolution
         [STAThread]
         static void Main()
         {
+            /**
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GUI());
+            **/
+
+            //Testar nedanför : Connection samt skapa shuffled deck med 52 kort. 
+            Deck deckTest = DAL.DBAccess.CreateDeck();
+            for(int i = 0; i < deckTest.cards.Count; i++)
+            {
+                Console.WriteLine(deckTest.cards[i].value);
+                Console.WriteLine(" Antal kort: " + deckTest.cards.Count);
+            }
         }
     }
 }
