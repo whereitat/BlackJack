7c3ee08497bcd5aa7a98ffc78f65a9c99861b1ee
whereitat <erik.hallkvist@gmail.com>
2016-10-26 18:20:26 +0200

    COMMIT

--- BlackJackSolution/BlackJackSolution/DAL/DBAccess.cs
+++ BlackJackSolution/BlackJackSolution/DAL/DBAccess.cs
@@ -195,14 +195,45 @@ namespace BlackJackSolution.DAL
             }
         }
 
-        public int GetMinBet()
+        public string[] GetBlackJackGame(int sessionId)
         {
+            try
+            {
+                SqlConnection connection = Connect();
+                SqlCommand command = new SqlCommand("EXEC [dbo].[GETBLACKJACKGAME] @USERNAME = '" + aname + "', @PASSWORD = '" + password + "'", connection);
+                command.ExecuteNonQuery();
+                SqlDataReader read = command.ExecuteReader();
+                string[] result = new string[4];
 
+                if (read.HasRows)
+                {
+                    if (read.FieldCount > 1)
+                    {
+                        while (read.Read())
+                        {
+                            result[0] = read.GetString(0);
+                            result[1] = read.GetString(1);
+                            result[2] = read.GetDouble(2).ToString();
+                            result[3] = read.GetString(3);
+                        }
+                    }
+                    else if (read.FieldCount == 1)
+                    {
+                        while (read.Read())
+                        {
+                            result[0] = read.GetString(0);
+                        }
+                    }
+                }
+                return result;
+            }
+            catch (SqlException e) //KOLLA DENNA SEN, VAD DEN SKA RETURNA
+            {
+                string ex = Logic.Utils.SqlExceptionUtility(e);
+                return null;
+            }
         }
-        public int GetMaxBet()
-        {
-
-        }
+        
         public string CreateGameRound(int bet, int result, string aname, int sessionid) //KLAR ISH, kanske vill förfina koden
         {
             try {
