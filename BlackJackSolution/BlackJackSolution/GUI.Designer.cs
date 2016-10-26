f0c317dd54dc04702acf6f579c972bf80fbcbea9
whereitat <erik.hallkvist@gmail.com>
2016-10-27 00:13:35 +0200

    Revert ".ico"
    This reverts commit 11219ca106df7ec75f92bded9f049a48ce8b79e2.

--- BlackJackSolution/BlackJackSolution/GUI.Designer.cs
+++ BlackJackSolution/BlackJackSolution/GUI.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
             this.MainPanel = new System.Windows.Forms.Panel();
             this.MainInfoGroupBox = new System.Windows.Forms.GroupBox();
             this.MainInfoLabel = new System.Windows.Forms.Label();
@@ -914,10 +913,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(946, 547);
-            this.Controls.Add(this.LoginPanel);
             this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.LoginPanel);
             this.Controls.Add(this.GamePanel);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "GUI";
             this.Text = "Blackjack";
